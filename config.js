fc84c2b9f1b283fe6886d86ea018de22331f0463
rajonbd1 <tg2452771@gmail.com>
2025-07-11 03:17:48 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js - Easily editable configuration for your redirect page
 
 const CONFIG = {
-    REDIRECT_URL: "https://www.google.com" // The URL where users will be redirected
+    REDIRECT_URL: "https://www.google.com", // The URL where users will be redirected
     OG_TITLE: "Earn $100 daily", // Title for social media sharing
     OG_DESCRIPTION: "Check out this amazing offer and start earning today!", // Description for social media sharing
     OG_IMAGE_URL: "https://i.imghippo.com/files/gbuB7452ddM.png", // Image for social media sharing (absolute URL)
